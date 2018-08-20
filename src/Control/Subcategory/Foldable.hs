69e06dfce35c3e15a2900bbc2b533c523160c7e9
Hiromi ISHII <konn.jinro@gmail.com>
2018-08-20 16:22:08 +0900

    Removes apps and tests

---
+++ src/Control/Subcategory/Foldable.hs
@@ -0,0 +1,8 @@
+module Control.Subcategory.Foldable where
+import Control.Subcategory.Functor
+
+class CFunctor f => CFoldable f where
+  cfoldMap :: (Cat f a, Monoid w) => (a -> w) -> f a -> w
+  cfold :: (Cat f w, Monoid w) => f w -> w
+
+
