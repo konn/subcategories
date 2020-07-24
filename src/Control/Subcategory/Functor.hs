107c35c78fd0af5c336f8e19912c71e9c1c67c71
Hiromi ISHII <konn.jinro@gmail.com>
2020-07-24 22:40:11 +0900

    Introduces `Constrained` type-class to avoid `CFunctor` constraint in `CFoldable`

--- src/Control/Subcategory/Functor.hs
+++ src/Control/Subcategory/Functor.hs
@@ -1,9 +1,14 @@
-{-# LANGUAGE InstanceSigs, TypeOperators #-}
+{-# LANGUAGE GADTs, InstanceSigs, KindSignatures, PatternSynonyms           #-}
+{-# LANGUAGE RankNTypes, RoleAnnotations, ScopedTypeVariables               #-}
+{-# LANGUAGE TemplateHaskell, TypeApplications, TypeFamilies, TypeOperators #-}
 module Control.Subcategory.Functor
-  ( CFunctor(..), (<$:>)
-  , defaultEmapConst, WrapFunctor(..), WrapMono(..)
-  , WrapIntContainer(..)
-  ) where
+  ( Constrained(..), CFunctor (..),
+    (<$:>),
+    defaultEmapConst,
+    WrapFunctor (..),
+    WrapMono (WrapMono, unwrapMono),
+  )
+where
 import qualified Control.Applicative             as App
 import           Control.Arrow                   (Arrow, ArrowMonad)
 import           Control.Exception               (Handler)
@@ -19,33 +24,34 @@ import           Data.Hashable                   (Hashable)
 import qualified Data.HashMap.Strict             as HM
 import qualified Data.HashSet                    as HS
 import qualified Data.IntMap                     as IM
-import qualified Data.IntSet                     as IS
-import           Data.Kind                       (Constraint)
+import           Data.Kind                       (Constraint, Type)
 import           Data.List.NonEmpty              (NonEmpty)
 import qualified Data.Map                        as Map
 import qualified Data.Monoid                     as Mon
-import           Data.MonoTraversable            (Element, MonoFunctor (..))
+import           Data.MonoTraversable            (Element, MonoFunctor (..),
+                                                  WrappedMono)
 import           Data.Ord                        (Down (..))
 import           Data.Proxy                      (Proxy)
-import           Data.Semigroup                  (Semigroup ((<>)))
 import qualified Data.Semigroup                  as Sem
 import qualified Data.Sequence                   as Seq
 import qualified Data.Set                        as Set
 import qualified Data.Tree                       as Tree
 import           Foreign.Ptr                     (Ptr)
 import           GHC.Conc                        (STM)
-import           GHC.Generics                    ((:*:), (:+:), (:.:), K1, M1)
-import           GHC.Generics                    (Par1, Rec1, U1, URec, V1)
+import           GHC.Generics                    ((:*:), (:+:), (:.:), K1, M1,
+                                                  Par1, Rec1, U1, URec, V1)
 import qualified System.Console.GetOpt           as GetOpt
 import           Text.ParserCombinators.ReadP    (ReadP)
 import           Text.ParserCombinators.ReadPrec (ReadPrec)
 
+
 infixl 4 <$:
 
-class CFunctor f where
-  type Cat f a :: Constraint
+class Constrained (f :: Type -> Type) where
+  type Cat f (a :: k) :: Constraint
   type Cat f a = ()
 
+class Constrained f => CFunctor f where
   emap :: (Cat f a, Cat f b) => (a -> b) -> f a -> f b
   default emap :: Functor f => (a -> b) -> f a -> f b
   emap = fmap
@@ -59,148 +65,249 @@ defaultEmapConst :: (CFunctor f, Cat f a, Cat f b) => a -> f b -> f a
 defaultEmapConst = emap . const
 {-# INLINE defaultEmapConst #-}
 
-newtype WrapFunctor f a = WrapFunctor { runFunctor :: f a }
+newtype WrapFunctor f a = WrapFunctor {runFunctor :: f a}
   deriving newtype (Functor, Applicative, App.Alternative, Monad)
 
-instance Functor f => CFunctor (WrapFunctor f) where
+instance Constrained (WrapFunctor f) where
   type Cat (WrapFunctor f) a = ()
-  emap :: forall a b. (a -> b) -> WrapFunctor f a -> WrapFunctor f b
+
+instance Functor f => CFunctor (WrapFunctor f) where
+  emap :: (a -> b) -> WrapFunctor f a -> WrapFunctor f b
   emap = fmap
   {-# INLINE emap #-}
-  (<$:) :: forall a b. a -> WrapFunctor f b -> WrapFunctor f a
+  (<$:) :: a -> WrapFunctor f b -> WrapFunctor f a
   (<$:) = (<$)
   {-# INLINE (<$:) #-}
 
+instance Constrained []
 instance CFunctor []
+instance Constrained Maybe
 instance CFunctor Maybe
+instance Constrained IO
 instance CFunctor IO
+instance Constrained Par1
 instance CFunctor Par1
+instance Constrained NonEmpty
 instance CFunctor NonEmpty
+instance Constrained ReadP
 instance CFunctor ReadP
+instance Constrained ReadPrec
 instance CFunctor ReadPrec
-instance CFunctor Down where
-  emap f (Down a) = Down $ f a
-  {-# INLINE emap #-}
-  (<$:) = defaultEmapConst
-  {-# INLINE (<$:) #-}
+
+
+instance Constrained Down
+instance CFunctor Down
+instance Constrained Mon.Product
 instance CFunctor Mon.Product
+
+instance Constrained Mon.Sum
 instance CFunctor Mon.Sum
+instance Constrained Mon.Dual
 instance CFunctor Mon.Dual
+
+instance Constrained Mon.Last
 instance CFunctor Mon.Last
+instance Constrained Mon.First
 instance CFunctor Mon.First
+
+instance Constrained STM
 instance CFunctor STM
+instance Constrained Handler
 instance CFunctor Handler
+
+instance Constrained Identity
 instance CFunctor Identity
+instance Constrained App.ZipList
 instance CFunctor App.ZipList
+instance Constrained GetOpt.ArgDescr
 instance CFunctor GetOpt.ArgDescr
+instance Constrained GetOpt.OptDescr
 instance CFunctor GetOpt.OptDescr
+instance Constrained GetOpt.ArgOrder
 instance CFunctor GetOpt.ArgOrder
+instance Constrained Sem.Option
 instance CFunctor Sem.Option
+
+instance Constrained Sem.Last
 instance CFunctor Sem.Last
+instance Constrained Sem.First
 instance CFunctor Sem.First
+
+instance Constrained Sem.Max
 instance CFunctor Sem.Max
+instance Constrained Sem.Min
 instance CFunctor Sem.Min
+
+instance Constrained Complex
 instance CFunctor Complex
+instance Constrained (Either a)
 instance CFunctor (Either a)
+
+instance Constrained V1
 instance CFunctor V1
+instance Constrained U1
 instance CFunctor U1
+
+instance Constrained ((,) a)
 instance CFunctor ((,) a)
+instance Constrained (SST.ST s)
 instance CFunctor (SST.ST s)
+
+instance Constrained (LST.ST s)
 instance CFunctor (LST.ST s)
+instance Constrained Proxy
 instance CFunctor Proxy
+
+instance Constrained (ArrowMonad a)
 instance Arrow a => CFunctor (ArrowMonad a)
+instance Constrained (App.WrappedMonad m)
 instance Monad m => CFunctor (App.WrappedMonad m)
+
+instance Constrained (Sem.Arg a)
 instance CFunctor (Sem.Arg a)
+instance Constrained (Rec1 f)
 instance Functor f => CFunctor (Rec1 f)
+
+instance Constrained (URec Char)
 instance CFunctor (URec Char)
+instance Constrained (URec Double)
 instance CFunctor (URec Double)
+
+instance Constrained (URec Float)
 instance CFunctor (URec Float)
+instance Constrained (URec Int)
 instance CFunctor (URec Int)
+
+instance Constrained (URec Word)
 instance CFunctor (URec Word)
+instance Constrained (URec (Ptr ()))
 instance CFunctor (URec (Ptr ()))
-instance CFunctor f => CFunctor (Mon.Alt f) where
+
+instance Constrained (Mon.Alt f) where
   type Cat (Mon.Alt f) a = Cat f a
+
+instance CFunctor f => CFunctor (Mon.Alt f) where
+
   emap f = Mon.Alt . emap f . Mon.getAlt
   {-# INLINE emap #-}
+
   (<$:) = defaultEmapConst
   {-# INLINE (<$:) #-}
+
+instance Constrained (Const m)
 instance CFunctor (Const m)
+instance Constrained (App.WrappedArrow a b)
 instance Arrow a => CFunctor (App.WrappedArrow a b)
+
+instance Constrained ((->) r)
 instance CFunctor ((->) r)
+instance Constrained (K1 i c)
 instance CFunctor (K1 i c)
+
+instance Constrained (f :+: g)
 instance (Functor f, Functor g) => CFunctor (f :+: g)
+instance Constrained (f :*: g)
 instance (Functor f, Functor g) => CFunctor (f :*: g)
+
+instance Constrained (f :.: g)
 instance (Functor f, Functor g) => CFunctor (f :.: g)
-instance (CFunctor f, CFunctor g) => CFunctor (SOP.Sum f g) where
+instance (Constrained f, Constrained g) => Constrained (SOP.Sum f g) where
   type Cat (SOP.Sum f g) a = (Cat f a, Cat g a)
+
+instance (CFunctor f, CFunctor g) => CFunctor (SOP.Sum f g) where
   emap f (SOP.InL a) = SOP.InL $ emap f a
+
   emap f (SOP.InR b) = SOP.InR $ emap f b
   {-# INLINE emap #-}
+
   (<$:) = defaultEmapConst
   {-# INLINE (<$:) #-}
-instance (CFunctor f, CFunctor g) => CFunctor (SOP.Product f g) where
+
+instance (Constrained f, Constrained g) => Constrained (SOP.Product f g) where
   type Cat (SOP.Product f g) a = (Cat f a, Cat g a)
+
+instance (CFunctor f, CFunctor g) => CFunctor (SOP.Product f g) where
   emap f (SOP.Pair a b) = SOP.Pair (emap f a) (emap f b)
   {-# INLINE emap #-}
+
   (<$:) = defaultEmapConst
   {-# INLINE (<$:) #-}
-instance (CFunctor f, CFunctor g) => CFunctor (SOP.Compose f g) where
+
+instance (Constrained (f ::Type -> Type), Constrained (g :: Type -> Type))
+  => Constrained (SOP.Compose f g) where
   type Cat (SOP.Compose f g) a = (Cat g a, Cat f (g a))
+
+instance (CFunctor f, CFunctor g) => CFunctor (SOP.Compose f g) where
   emap f (SOP.Compose a) = SOP.Compose $ emap (emap f) a
   (<$:) = defaultEmapConst
+
   {-# INLINE (<$:) #-}
+
+instance Constrained (M1 i c f)
 instance Functor f => CFunctor (M1 i c f)
+
+instance Constrained Seq.Seq
 instance CFunctor Seq.Seq
 
-data WrapMono a b where
-  WrapMono :: {unwrapMono :: {-# UNPACK #-}!a} -> WrapMono a (Element a)
+type role WrapMono representational nominal
+-- | Similar to 'WrappedMono' from @mono-traversable,
+--   but uses @newtype@ instaed of GADTs, which is efficient.
+--   To restrict the construction, we hide genuine constructor
+--   and expose the constrained pattern synonym 'WrapMono' and
+--   specifies type roles tightly (note: the role for @mono@
+--   should NOT be representational honestly; indeed, @WrapMono mono a@
+--   could be coerced to @WrapMono mono' a@ iff @mono@ and @mono' are
+--   representationally equivalent __AND__ @Element a ~ Element a@.)
+newtype WrapMono mono b = WrapMono' mono
 
-instance {-# OVERLAPPABLE #-}  MonoFunctor a => CFunctor (WrapMono a) where
-  type Cat (WrapMono a) b = b ~ Element a
+pattern WrapMono :: b ~ Element mono => b ~ Element mono => mono -> WrapMono mono b
+pattern WrapMono {unwrapMono} = WrapMono' unwrapMono
+
+instance Constrained (WrappedMono mono) where
+  type Cat (WrappedMono mono) a = a ~ Element mono
+
+instance Constrained (WrapMono mono) where
+  type Cat (WrapMono mono) b = b ~ Element mono
+
+instance {-# OVERLAPPABLE #-} MonoFunctor a
+      => CFunctor (WrapMono a) where
   emap f = WrapMono . omap f . unwrapMono
+
   {-# INLINE emap #-}
   (<$:) = defaultEmapConst
+
   {-# INLINE (<$:) #-}
 
-data WrapIntContainer a b where
-  WrapIntContainer :: {unwrapIntContainer :: {-# UNPACK #-} !a } -> WrapIntContainer a (Element a)
 
-instance (b ~ Element a, Semigroup a) => Semigroup (WrapIntContainer a b) where
-  WrapIntContainer a <> WrapIntContainer b = WrapIntContainer $ a <> b
-
-instance (b ~ Element a, Monoid a) => Monoid (WrapIntContainer a b) where
-  mempty = WrapIntContainer mempty
-  {-# INLINE mempty #-}
-  mappend (WrapIntContainer a) (WrapIntContainer b) =
-    WrapIntContainer $ a `mappend` b
-  {-# INLINE mappend #-}
-
-instance CFunctor (WrapIntContainer IS.IntSet) where
-  type Cat (WrapIntContainer IS.IntSet) b = b ~ Int
-  emap f = WrapIntContainer . IS.map f . unwrapIntContainer
-  {-# INLINE emap #-}
-  (<$:) = defaultEmapConst
-  {-# INLINE (<$:) #-}
-
+instance Constrained IM.IntMap
 instance CFunctor IM.IntMap
+
+instance Constrained (Map.Map k)
 instance Ord k => CFunctor (Map.Map k)
 
-instance CFunctor Set.Set where
+instance Constrained Set.Set where
   type Cat Set.Set a = Ord a
+
+instance CFunctor Set.Set where
   emap = Set.map
   {-# INLINE emap #-}
   (<$:) = defaultEmapConst
   {-# INLINE (<$:) #-}
 
-instance CFunctor HS.HashSet where
+instance Constrained HS.HashSet where
   type Cat HS.HashSet a = (Hashable a, Eq a)
+
+instance CFunctor HS.HashSet where
   emap = HS.map
   {-# INLINE emap #-}
   (<$:) = defaultEmapConst
   {-# INLINE (<$:) #-}
 
+instance Constrained (HM.HashMap k)
 instance CFunctor (HM.HashMap k)
+instance Constrained Tree.Tree
 instance CFunctor Tree.Tree
+
 
 infixl 4 <$:>
 (<$:>) :: (CFunctor f, Cat f a, Cat f b) => (a -> b) -> f a -> f b
