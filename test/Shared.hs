b70d9d3c0603d94262e2f4b22fe463f997455107
Hiromi ISHII <konn.jinro@gmail.com>
2020-08-02 19:13:30 +0900

    Changes to report error-location in inspection testing

--- test/Shared.hs
+++ test/Shared.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Shared where
 import Control.Exception
+import Data.Maybe          (listToMaybe)
+import GHC.Stack           (callStack)
+import GHC.Stack.Types     (getCallStack)
 import Language.Haskell.TH
 import Test.Inspection
 import Test.Tasty.HUnit
@@ -9,7 +12,7 @@ checkInspection
   :: HasCallStack => Result -> Assertion
 checkInspection Success{} = pure ()
 checkInspection (Failure msg) =
-  throwIO $ HUnitFailure Nothing msg
+  throwIO $ HUnitFailure (fmap snd $ listToMaybe $ getCallStack callStack) msg
 
 inspecting :: String -> Obligation -> Q Exp
 inspecting desc reg =
