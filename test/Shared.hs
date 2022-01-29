9f2d785deeee194ba08e5f70998cdca1a186dd95
Hiromi ISHII <konn.jinro@gmail.com>
2022-01-29 19:16:07 +0900

    Test cases: simplified subsumption

--- test/Shared.hs
+++ test/Shared.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
 module Shared where
 import Control.Exception
 import Data.Maybe          (listToMaybe)
@@ -17,3 +18,19 @@ checkInspection (Failure msg) =
 inspecting :: String -> Obligation -> Q Exp
 inspecting desc reg =
   [|testCase desc $ checkInspection $(inspectTest reg)|]
+
+data GHCVer = GHC8_8 | GHC8_10 | GHC9_0 | GHC9_2
+  deriving (Show, Eq, Ord)
+
+ghcVer :: GHCVer
+#if __GLASGOW_HASKELL__ == 902
+ghcVer = GHC9_2
+#elif __GLASGOW_HASKELL__ == 900
+ghcVer = GHC9_0
+#elif __GLASGOW_HASKELL__ == 810
+ghcVer = GHC8_10
+#elif __GLASGOW_HASKELL__ == 808
+ghcVer = GHC8_8
+#else
+ghcVer = error "Coudld not determine GHC Version: __GLASGOW_HASKELL__"
+#endif
